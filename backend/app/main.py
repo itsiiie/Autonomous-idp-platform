aabf2e2996a68588333016f41df19f8ce39b2d61
shashank <mdpkdy@gmail.com>
2026-02-13 15:42:26 +0530

    feat: add uvicorn ASGI server

---
+++ backend/app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from routes import auth
+
+app = FastAPI()
+
+app.include_router(auth.router)
+
+
+@app.get("/")
+def root():
+    return {"message": "IDP Platform Running 🚀"}
