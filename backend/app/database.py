ad0f876771a1ed9215dd10551ba5f4bb69c5c2ad
shashank <mdpkdy@gmail.com>
2026-02-13 15:31:53 +0530

    feat: add password hashing and JWT dependencies

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,12 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 import os
 
-DB_USER = "mdpkdy"
-DB_PASSWORD = "143isgame"
-DB_HOST = "idp-platform-db.c3ky84uwe8jm.ap-south-1.rds.amazonaws.com"
-DB_PORT = "5432"
-DB_NAME = "idpdb"
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
 DATABASE_URL = (
     f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
@@ -14,6 +17,10 @@ DATABASE_URL = (
 
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
